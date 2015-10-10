f74067244097d903b728e99be49c7494007f99b9
Marijan Šuflaj <msufflaj32@gmail.com>
2015-10-10 21:50:15 +0200

    Added custom headers matcher for VCR

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,12 @@
 <?php
 
-\VCR\VCR::configure()->setCassettePath('tests/fixtures');
+use VCR\Request;
+
+\VCR\VCR::configure()
+    ->setCassettePath('tests/fixtures/')
+    ->addRequestMatcher(
+        'headers',
+        function (Request $first, Request $second) {
+            return array_filter($first->getHeaders()) == array_filter($second->getHeaders());
+        }
+    );
