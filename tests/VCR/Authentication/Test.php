c90c35dbacfa40a387c206e315ce97d1e3465a41
Marijan Šuflaj <msufflaj32@gmail.com>
2015-10-18 16:14:15 +0200

    Authentication/can_use_just_a_token

--- tests/VCR/Authentication/Test.php
+++ tests/VCR/Authentication/Test.php
@@ -2,6 +2,7 @@
 
 namespace CurrencyCloud\Tests\VCR\Authentication;
 
+use CurrencyCloud\Model\Beneficiaries;
 use CurrencyCloud\Tests\BaseCurrencyCloudTestCase;
 
 class Test extends BaseCurrencyCloudTestCase
@@ -19,5 +20,28 @@ class Test extends BaseCurrencyCloudTestCase
         $client->authenticate()->close();
 
         $this->assertNull($client->getSession()->getAuthToken());
+    }
+
+    /**
+     * @vcr Authentication/can_use_just_a_token.yaml
+     * @test
+     */
+    public function canUseJustToken()
+    {
+
+        $client = $this->getClient();
+        $client->getSession()->setAuthToken('2ec8a86c8cf6e0378a20ca6793f3260c');
+
+
+        $beneficiaries = $client->beneficiaries()->find();
+
+        $this->assertTrue($beneficiaries instanceof Beneficiaries);
+        $this->assertCount(0, $beneficiaries->getBeneficiaries());
+
+        $dummy = json_decode(
+            '{"beneficiaries":[],"pagination":{"total_entries":0,"total_pages":1,"current_page":1,"per_page":25,"previous_page":-1,"next_page":-1,"order":"created_at","order_asc_desc":"asc"}}',
+            true
+        );
+        $this->validateObjectStrictName($beneficiaries->getPagination(), $dummy['pagination']);
     }
 }
