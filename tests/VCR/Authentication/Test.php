46b9fbce71fb506c4c594f1fcdef897b057c4da1
Marijan Šuflaj <msufflaj32@gmail.com>
2015-10-21 01:05:49 +0200

    Authentication/happens_lazily implemented

--- tests/VCR/Authentication/Test.php
+++ tests/VCR/Authentication/Test.php
@@ -29,8 +29,7 @@ class Test extends BaseCurrencyCloudTestCase
     public function canUseJustToken()
     {
 
-        $client = $this->getClient();
-        $client->getSession()->setAuthToken('2ec8a86c8cf6e0378a20ca6793f3260c');
+        $client = $this->getAuthenticatedClient('2ec8a86c8cf6e0378a20ca6793f3260c');
 
 
         $beneficiaries = $client->beneficiaries()->find();
@@ -43,5 +42,27 @@ class Test extends BaseCurrencyCloudTestCase
             true
         );
         $this->validateObjectStrictName($beneficiaries->getPagination(), $dummy['pagination']);
+    }
+
+    /**
+     * @vcr Authentication/happens_lazily.yaml
+     * @test
+     */
+    public function happensLazily()
+    {
+        $client = $this->getClient();
+        $client->getSession()->setAuthToken(null);
+
+        $beneficiaries = $client->beneficiaries()->find();
+
+        $this->assertTrue($beneficiaries instanceof Beneficiaries);
+        $this->assertCount(0, $beneficiaries->getBeneficiaries());
+
+        $dummy = json_decode(
+            '{"beneficiaries":[],"pagination":{"total_entries":0,"total_pages":1,"current_page":1,"per_page":25,"previous_page":-1,"next_page":-1,"order":"created_at","order_asc_desc":"asc"}}',
+            true
+        );
+        $this->validateObjectStrictName($beneficiaries->getPagination(), $dummy['pagination']);
+        $this->assertEquals('038022bcd2f372cac7bab448db7b5c3b', $client->getSession()->getAuthToken());
     }
 }
