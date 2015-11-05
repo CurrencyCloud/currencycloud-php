0253360564ac1d33f4559a041e75dcb7ccbad005
Marijan Šuflaj <msufflaj32@gmail.com>
2015-11-05 22:27:57 +0100

    Extra CurrencyCloudTest tests

--- tests/Core/CurrencyCloudTest.php
+++ tests/Core/CurrencyCloudTest.php
@@ -16,10 +16,13 @@ use CurrencyCloud\EntryPoint\RatesEntryPoint;
 use CurrencyCloud\EntryPoint\ReferenceEntryPoint;
 use CurrencyCloud\EntryPoint\SettlementsEntryPoint;
 use CurrencyCloud\EntryPoint\TransactionsEntryPoint;
+use CurrencyCloud\Model\Account;
+use CurrencyCloud\Model\Payment;
 use CurrencyCloud\Session;
 use CurrencyCloud\Tests\BaseCurrencyCloudTestCase;
 use GuzzleHttp\Psr7\Response;
 use LogicException;
+use RuntimeException;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 
 class CurrencyCloudTest extends BaseCurrencyCloudTestCase
@@ -55,17 +58,25 @@ class CurrencyCloudTest extends BaseCurrencyCloudTestCase
     {
         $transactionsData = '{"id":"c5a990eb-d4d7-482f-bfb1-695261fb1e4d","balance_id":"c5f1f54e-d6d8-4140-8110-f5b99bbc80c3","account_id":"7b9757a8-eee9-4572-86e6-77f4d711eaa6","currency":"USD","amount":"1000.00","balance_amount":"2000.00","type":"credit","action":"conversion","related_entity_type":"conversion","related_entity_id":"ConversionUUID","related_entity_short_reference":"140416-GGJBNQ001","status":"completed","reason":"Reason for Transaction","settles_at":"2014-01-12T12:24:19+00:00","created_at":"2014-01-12T12:24:19+00:00","updated_at":"2014-01-12T12:24:19+00:00","completed_at":"2014-01-12T12:24:19+00:00"}';
 
-        $contactsData = '{"login_id":"john.smith","id":"543477161-91de-012f-e284-1e0030c7f352","your_reference":"ACME12345","first_name":"John","last_name":"Smith","account_id":"87077161-91de-012f-e284-1e0030c7f352","account_name":"Company PLC","status":"enabled","phone_number":"06554 87845","mobile_phone_number":"07564 534 54","locale":"en-US","timezone":"Europe/London","email_address":"john.smith@company.com","date_of_birth":"1980-01-22","created_at":"2014-01-12T00:00:00+00:00","updated_at":"2014-01-12T00:00:00+00:00"}';
+        $paymentsData = '{"id":"543477161-91de-012f-e284-1e0030c7f3123","short_reference":"140416-GGJANQ001","beneficiary_id":"543477161-91de-012f-e284-1e0030c7f352","conversion_id":"543477161-91de-012f-e284-1e0030c7f352","amount":"1250000.00","currency":"GBP","status":"ready_to_send","payment_type":"regular","reference":"INVOICE 9876","reason":"Salary for March","payment_date":"2014-01-12T00:00:00+00:00","transferred_at":"2014-01-12T13:00:00+00:00","authorisation_steps_required":"0","creator_contact_id":"ab3477161-91de-012f-e284-1e0030c7f35c","last_updater_contact_id":"ab3477161-91de-012f-e284-1e0030c7f35c","failure_reason":"","payer_id":"","payer_details_source":"","created_at":"2014-01-12T12:24:19+00:00","updated_at":"2014-01-12T12:24:19+00:00"}';
 
         $mockedClient = $this->getMock(\GuzzleHttp\Client::class, ['request']);
 
         $mockedClient->expects($this->exactly(2))->method('request')->withConsecutive(
             [$this->equalTo('GET'), 'https://devapi.thecurrencycloud.com/v2/transactions/hi?on_behalf_of=aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'],
-            [$this->equalTo('GET'), 'https://devapi.thecurrencycloud.com/v2/contacts/hi?on_behalf_of=aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa']
+            [$this->equalTo('POST'), 'https://devapi.thecurrencycloud.com/v2/payments/me/delete', $this->equalTo([
+                'headers' => [
+                    'X-Auth-Token' => null
+                ],
+                'form_params' => [
+                    'on_behalf_of' => 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'
+                ],
+                'http_errors' => false
+            ])]
         )
             ->will($this->onConsecutiveCalls(
                 new Response(200, [], $transactionsData),
-                new Response(200, [], $contactsData)
+                new Response(200, [], $paymentsData)
             ));
 
         /* @var \GuzzleHttp\Client $mockedClient */
@@ -74,12 +85,16 @@ class CurrencyCloudTest extends BaseCurrencyCloudTestCase
         $currencyCloudClient = new Client($session, $mockedClient, new EventDispatcher());
         $client = CurrencyCloud::createDefault($session, $currencyCloudClient);
 
-        $client->onBehalfOf('aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', function (CurrencyCloud $client) use ($transactionsData, $contactsData) {
+        $client->onBehalfOf('aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', function (CurrencyCloud $client) use ($transactionsData, $paymentsData) {
             $transaction = $client->transactions()->retrieve('hi');
             $this->validateObjectStrictName($transaction, json_decode($transactionsData, true));
-            $contact = $client->contacts()->retrieve('hi');
-            $this->validateObjectStrictName($contact, json_decode($contactsData, true));
+            $payment = new Payment();
+            $this->setIdProperty($payment, 'me');
+            $contact = $client->payments()->delete($payment);
+            $this->validateObjectStrictName($contact, json_decode($paymentsData, true));
         });
+
+        $this->assertNull($session->getOnBehalfOf());
     }
 
     /**
@@ -100,5 +115,49 @@ class CurrencyCloudTest extends BaseCurrencyCloudTestCase
         $client->onBehalfOf('aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', function (CurrencyCloud $client) {
             $client->transactions()->retrieve('hi', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab');
         });
+    }
+
+    /**
+     * @test
+     */
+    public function canOnBehalfOfFromMethod()
+    {
+        $mockedClient = $this->getMock(\GuzzleHttp\Client::class, ['request']);
+
+        $transactionsData = '{"id":"c5a990eb-d4d7-482f-bfb1-695261fb1e4d","balance_id":"c5f1f54e-d6d8-4140-8110-f5b99bbc80c3","account_id":"7b9757a8-eee9-4572-86e6-77f4d711eaa6","currency":"USD","amount":"1000.00","balance_amount":"2000.00","type":"credit","action":"conversion","related_entity_type":"conversion","related_entity_id":"ConversionUUID","related_entity_short_reference":"140416-GGJBNQ001","status":"completed","reason":"Reason for Transaction","settles_at":"2014-01-12T12:24:19+00:00","created_at":"2014-01-12T12:24:19+00:00","updated_at":"2014-01-12T12:24:19+00:00","completed_at":"2014-01-12T12:24:19+00:00"}';
+
+        $mockedClient->expects($this->once())->method('request')->withConsecutive(
+            [$this->equalTo('GET'), 'https://devapi.thecurrencycloud.com/v2/transactions/hi?on_behalf_of=aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa']
+        )
+            ->will($this->onConsecutiveCalls(
+                new Response(200, [], $transactionsData)
+            ));
+
+        /* @var \GuzzleHttp\Client $mockedClient */
+
+        $session = new Session(Session::ENVIRONMENT_DEMONSTRATION, 'a', 'b');
+        $currencyCloudClient = new Client($session, $mockedClient, new EventDispatcher());
+        $client = CurrencyCloud::createDefault($session, $currencyCloudClient);
+
+        $transaction = $client->transactions()->retrieve('hi', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa');
+        $this->validateObjectStrictName($transaction, json_decode($transactionsData, true));
+    }
+
+    /**
+     * @test
+     */
+    public function exceptionWhenEntityIsNotManagedByManager()
+    {
+        $mockedClient = $this->getMock(\GuzzleHttp\Client::class, ['request']);
+
+        $this->setExpectedException(RuntimeException::class, 'Entity is not managed by entity manager and therefore can not be updated');
+
+        /* @var \GuzzleHttp\Client $mockedClient */
+
+        $session = new Session(Session::ENVIRONMENT_DEMONSTRATION, 'a', 'b');
+        $currencyCloudClient = new Client($session, $mockedClient, new EventDispatcher());
+        $client = CurrencyCloud::createDefault($session, $currencyCloudClient);
+
+        $client->accounts()->update(new Account());
     }
 }
