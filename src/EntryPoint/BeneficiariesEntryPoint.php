125f718a54c547e66cc340b73974c8c790c25b32
Ollie Rattue <ollie@toomanytabs.com>
2016-05-25 17:20:40 +0200

    Tweaked convertBeneficiaryToRequest() parameters so they appear on one line rather than multiple to match the style of the other functions

--- src/EntryPoint/BeneficiariesEntryPoint.php
+++ src/EntryPoint/BeneficiariesEntryPoint.php
@@ -127,11 +127,8 @@ class BeneficiariesEntryPoint extends AbstractEntityEntryPoint
      *
      * @return array
      */
-    protected function convertBeneficiaryToRequest(
-        Beneficiary $beneficiary,
-        $convertForValidate = false,
-        $convertForUpdate = false
-    ) {
+    protected function convertBeneficiaryToRequest(Beneficiary $beneficiary, $convertForValidate = false, $convertForUpdate = false)
+	{
         $isDefaultBeneficiary = $beneficiary->isDefaultBeneficiary();
         $common = [
             'bank_country' => $beneficiary->getBankCountry(),
