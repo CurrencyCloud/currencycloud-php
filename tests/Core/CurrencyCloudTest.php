b609ed2f3c0db7b038eff60261d1b5dc557c3732
Francesco Boffa <fra.boffa@gmail.com>
2017-12-04 11:10:56 +0000

    Fixed User-Agent test

--- tests/Core/CurrencyCloudTest.php
+++ tests/Core/CurrencyCloudTest.php
@@ -67,7 +67,7 @@ class CurrencyCloudTest extends BaseCurrencyCloudTestCase
             [$this->equalTo('POST'), 'https://devapi.currencycloud.com/v2/payments/me/delete', $this->equalTo([
                 'headers' => [
                     'X-Auth-Token' => null,
-                    'User-Agent' => 'CurrencyCloud/2.0 PHP/0.9.0'
+                    'User-Agent' => 'CurrencyCloudSDK/2.0 PHP/0.11.0'
                 ],
                 'form_params' => [
                     'on_behalf_of' => 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'
