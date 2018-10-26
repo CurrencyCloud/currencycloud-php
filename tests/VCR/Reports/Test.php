d5fa570925a78cfe79c7667220b9d4ede8046122
cpacalevcc <corneliu.pacalev@currencycloud.com>
2018-10-26 10:42:37 +0300

    [TCC-15477] fix typo

--- tests/VCR/Reports/Test.php
+++ tests/VCR/Reports/Test.php
@@ -13,11 +13,11 @@ class Test extends BaseCurrencyCloudVCRTestCase{
     public function canCreateConversionReport()
     {
 
-        $conversionReportCritetira = new ConversionReportCriteria();
-        $conversionReportCritetira->setBuyCurrency("EUR")
+        $conversionReportCriteria = new ConversionReportCriteria();
+        $conversionReportCriteria->setBuyCurrency("EUR")
             ->setSellCurrency("GBP");
 
-        $report = $this->getAuthenticatedClient()->reports()->createConversionReport($conversionReportCritetira);
+        $report = $this->getAuthenticatedClient()->reports()->createConversionReport($conversionReportCriteria);
 
         $dummy = json_decode(
             '{"id":"ea8da492-b03d-4330-b279-b5e4a25804a0","short_reference":"RP-3256961-MLEPRN","description":"Test report for Conversion","search_params":{"buy_currency":"EUR","sell_currency":"GBP","scope":"own"},"report_type":"conversion","status":"processing","failure_reason":null,"expiration_date":null,"report_url":"","account_id":"bf5b1007-b364-43cc-b3d6-9f2d1be75297","contact_id":"ba33d76a-4a7f-4cb3-afa8-5678d5bc712a","created_at":"2018-10-25T10:16:32+00:00","updated_at":"2018-10-25T10:16:32+00:00"}',
