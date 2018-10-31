d045220134d0d64479a2567377535c4ec512a87c
cpacalevcc <corneliu.pacalev@currencycloud.com>
2018-10-31 10:58:54 +0200

    [TCC-15752] add entry point test for payment confirmation

--- tests/EntryPoint/PaymentsEntryPointTest.php
+++ tests/EntryPoint/PaymentsEntryPointTest.php
@@ -452,4 +452,30 @@ class PaymentsEntryPointTest extends BaseCurrencyCloudTestCase
 
         $this->validateObjectStrictName($list[0], json_decode($data, true)['payments'][0]);
     }
+
+    /**
+     * @test
+     */
+    public function canGetPaymentConfirmation(){
+        $data = '{"id":"e6b30f2d-0088-4d99-bb47-c6b136fcf447","payment_id":"796e0d7d-bae6-4d8a-b217-3cf9ee80a350","account_id":"72970a7c-7921-431c-b95f-3438724ba16f","short_reference":"PC-2436231-LYODVS","status":"failed","confirmation_url":"","created_at":"2018-08-03T13:18:23+00:00","updated_at":"2018-08-03T13:18:23+00:00","expires_at":null}';
+
+        $entryPoint = new PaymentsEntryPoint(new SimpleEntityManager(),
+            $this->getMockedClient(
+                json_decode($data),
+                'GET',
+                'payments/796e0d7d-bae6-4d8a-b217-3cf9ee80a350/confirmation',
+                [
+                    'on_behalf_of' => null
+                ]
+            )
+        );
+
+        $paymentConfirmation = $entryPoint->retrieveConfirmation('796e0d7d-bae6-4d8a-b217-3cf9ee80a350');
+        $dummy = json_decode($data, true);
+
+        $this->assertSame($dummy['payment_id'], $paymentConfirmation->getPaymentId());
+        $this->assertSame($dummy['account_id'], $paymentConfirmation->getAccountId());
+        $this->assertSame($dummy['short_reference'], $paymentConfirmation->getShortReference());
+        $this->assertSame($dummy['status'], $paymentConfirmation->getStatus());
+    }
 }
