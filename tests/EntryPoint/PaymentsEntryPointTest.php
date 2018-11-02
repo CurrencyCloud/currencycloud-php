382e8f1d463a30d58253b244213d10a9bdc5d7d1
cpacalevcc <corneliu.pacalev@currencycloud.com>
2018-11-02 10:57:03 +0200

    [TCC-15620] update tests

--- tests/EntryPoint/PaymentsEntryPointTest.php
+++ tests/EntryPoint/PaymentsEntryPointTest.php
@@ -458,9 +458,9 @@ class PaymentsEntryPointTest extends BaseCurrencyCloudTestCase
      */
     public function canGetPaymentSubmission(){
         $data = '{
-            "status": null,
-            "mt103": null,
-            "submission_ref": null
+            "mt103": "{1:F01TCCLGB20AXXX0090000004}{2:I103BARCGB22XXXXN}{4: :20:20160617-ZSYWVY :23B:CRED :32A:160617GBP3000,0 :33B:GBP3000,0 :50K:/150618-00026 PCOMAPNY address New-York Province 555222 GB :53B:/20060513071472 :57C://SC200605 :59:/200605000 First Name Last Name e03036bf6c325dd12c58 London GB :70:test reference Test reason Payment group: 0160617-ZSYWVY :71A:SHA -}",
+            "status": "pending",
+            "submission_ref": "MXGGYAGJULIIQKDV"
         }';
 
         $entryPoint = new PaymentsEntryPoint(new SimpleEntityManager(), $this->getMockedClient(
