ce7eea9e6fceaa4b3f1daea8feef741353e29e64
cpacalevcc <corneliu.pacalev@currencycloud.com>
2018-11-02 14:30:09 +0200

    [TCC-15752] updated tests with more data

--- tests/EntryPoint/PaymentsEntryPointTest.php
+++ tests/EntryPoint/PaymentsEntryPointTest.php
@@ -457,7 +457,17 @@ class PaymentsEntryPointTest extends BaseCurrencyCloudTestCase
      * @test
      */
     public function canGetPaymentConfirmation(){
-        $data = '{"id":"e6b30f2d-0088-4d99-bb47-c6b136fcf447","payment_id":"796e0d7d-bae6-4d8a-b217-3cf9ee80a350","account_id":"72970a7c-7921-431c-b95f-3438724ba16f","short_reference":"PC-2436231-LYODVS","status":"failed","confirmation_url":"","created_at":"2018-08-03T13:18:23+00:00","updated_at":"2018-08-03T13:18:23+00:00","expires_at":null}';
+        $data = '{
+            "id": "d7d5c073-7aac-415a-b2cd-f3f4942ca164",
+            "payment_id": "3c66de91-0083-4e8f-aff7-a2b5250f6aa8",
+            "account_id": "bf5b1007-b364-43cc-b3d6-9f2d1be75297",
+            "short_reference": "PC-9387530-VFDXNJ",
+            "status": "completed",
+            "confirmation_url": "https://ccycloud-payment-confirmations-prod-demo1.s3.eu-west-1.amazonaws.com/payment_confirmations/d7d5c073-7aac-415a-b2cd-f3f4942ca164/181102-XYJHCH001.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=abcxyz12345&X-Amz-Date=20181102T121904Z&X-Amz-Expires=172800&X-Amz-SignedHeaders=host&X-Amz-Security-Token=wxyzabc123&X-Amz-Signature=abcdef1234",
+            "created_at": "2018-11-02T09:02:57+00:00",
+            "updated_at": "2018-11-02T09:02:58+00:00",
+            "expires_at": "2018-11-04T00:00:00+00:00"
+        }';
 
         $entryPoint = new PaymentsEntryPoint(new SimpleEntityManager(),
             $this->getMockedClient(
