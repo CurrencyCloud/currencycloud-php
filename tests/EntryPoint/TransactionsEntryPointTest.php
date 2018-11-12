674aee94586fdd476655f3ab2b13cb378b610597
cpacalevcc <corneliu.pacalev@currencycloud.com>
2018-11-12 09:55:35 +0200

    [TCC-15927] add entrypoint sender details test

--- tests/EntryPoint/TransactionsEntryPointTest.php
+++ tests/EntryPoint/TransactionsEntryPointTest.php
@@ -207,4 +207,43 @@ class TransactionsEntryPointTest extends BaseCurrencyCloudTestCase
 
         $this->validateObjectStrictName($list[0], json_decode($data, true)['transactions'][0]);
     }
+
+    /**
+     * @test
+     */
+    public function canRetrieveSenderDetails(){
+        $data = '{
+            "id": "e68301d3-5b04-4c1d-8f8b-13a9b8437040",
+            "amount": "1701.51",
+            "currency": "EUR",
+            "additional_information": "USTRD-0001",
+            "value_date": "2018-07-04T00:00:00+00:00",
+            "sender": "FR7615589290001234567890113, CMBRFR2BARK, Debtor, FR, Centre ville",
+            "receiving_account_number": null,
+            "receiving_account_iban": "GB99OXPH94665099600083",
+            "created_at": "2018-07-04T14:57:38+00:00",
+            "updated_at": "2018-07-04T14:57:39+00:00"
+        }';
+
+        $entryPoint = new TransactionsEntryPoint($this->getMockedClient(
+            json_decode($data),
+            'GET',
+            'transactions/sender/12345678-abcd-1234-abcd-123456789012',
+            [
+                'on_behalf_of' => null
+            ]
+        ));
+
+        $transactionSender = $entryPoint->retrieveSender('12345678-abcd-1234-abcd-123456789012');
+
+        $dummy = json_decode($data, true);
+
+        $this->assertSame($dummy['id'], $transactionSender->getId());
+        $this->assertSame($dummy['amount'], $transactionSender->getAmount());
+        $this->assertSame($dummy['currency'], $transactionSender->getCurrency());
+        $this->assertSame($dummy['additional_information'], $transactionSender->getAdditionalInformation());
+        $this->assertSame($dummy['sender'], $transactionSender->getSender());
+        $this->assertSame($dummy['receiving_account_number'], $transactionSender->getReceivingAcountNumber());
+        $this->assertSame($dummy['receiving_account_iban'], $transactionSender->getReceivingAcountIban());
+    }
 }
