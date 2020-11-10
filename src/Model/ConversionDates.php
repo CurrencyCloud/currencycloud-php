dcec38b053ffef689ccc18ec53b06a60740f8d64
jonathancouchman <jonathan.couchman@currencycloud.com>
2020-11-10 16:39:01 +0000

    Updates get conversion dates to add first_conversion_cutoff_datetime and optimize_liquidity_conversion_date to response

--- src/Model/ConversionDates.php
+++ src/Model/ConversionDates.php
@@ -19,20 +19,34 @@ class ConversionDates
      * @var DateTime
      */
     private $defaultConversionDay;
+    /**
+     * @var DateTime
+     */
+    private $firstConversionCutoffDatetime;
+    /**
+     * @var DateTime
+     */
+    private $optimizeLiquidityConversionDate;
 
     /**
      * @param InvalidConversionDate[] $invalidConversionDates
      * @param DateTime $firstConversionDay
      * @param DateTime $defaultConversionDay
+     * @param DateTime $firstConversionCutoffDatetime
+     * @param DateTime $optimizeLiquidityConversionDate
      */
     public function __construct(
         array $invalidConversionDates,
         DateTime $firstConversionDay,
-        DateTime $defaultConversionDay
+        DateTime $defaultConversionDay,
+        DateTime $firstConversionCutoffDatetime,
+        DateTime $optimizeLiquidityConversionDate
     ) {
         $this->invalidConversionDates = $invalidConversionDates;
         $this->firstConversionDay = $firstConversionDay;
         $this->defaultConversionDay = $defaultConversionDay;
+        $this->firstConversionCutoffDatetime = $firstConversionCutoffDatetime;
+        $this->optimizeLiquidityConversionDate = $optimizeLiquidityConversionDate;
     }
 
     /**
@@ -58,4 +72,22 @@ class ConversionDates
     {
         return $this->defaultConversionDay;
     }
+
+    /**
+     * @return DateTime
+     */
+    public function getFirstConversionCutoffDatetime()
+    {
+        return $this->firstConversionCutoffDatetime;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getOptimizeLiquidityConversionDate()
+    {
+        return $this->optimizeLiquidityConversionDate;
+    }
+
+
 }
