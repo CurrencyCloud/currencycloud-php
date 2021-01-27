bf23c96cc1d57b34cfd91ccc2779b7c29f98e696
jonathancouchman <44161978+jonathancouchman@users.noreply.github.com>
2021-01-27 10:19:16 +0000

    Sdkphp 73 payment purpose codes test (#108)
    * Adds test case for payment purpose codes

--- tests/EntryPoint/ReferencesEntryPointTest.php
+++ tests/EntryPoint/ReferencesEntryPointTest.php
@@ -377,4 +377,212 @@ class ReferencesEntryPointTest extends BaseCurrencyCloudTestCase
         $this->assertSame("5.25", $rules[0]->getFeeAmount());
         $this->assertSame("GBP", $rules[0]->getFeeCurrency());
     }
+
+    /**
+     * @test
+     */
+    public function canGetPaymenPurposeCode()
+    {
+        $data = '{
+    "purpose_codes": [
+        {
+            "bank_account_country": "IN",
+            "currency": "INR",
+            "entity_type": "individual",
+            "purpose_code": "travel",
+            "purpose_description": "Travel"
+        },
+        {
+            "bank_account_country": "IN",
+            "currency": "INR",
+            "entity_type": "individual",
+            "purpose_code": "services",
+            "purpose_description": "Information service charges"
+        },
+        {
+            "bank_account_country": "IN",
+            "currency": "INR",
+            "entity_type": "individual",
+            "purpose_code": "education",
+            "purpose_description": "Education-related student expenses"
+        },
+        {
+            "bank_account_country": "IN",
+            "currency": "INR",
+            "entity_type": "individual",
+            "purpose_code": "office",
+            "purpose_description": "Representative office expenses"
+        },
+        {
+            "bank_account_country": "IN",
+            "currency": "INR",
+            "entity_type": "individual",
+            "purpose_code": "delivery",
+            "purpose_description": "Delivery fees for goods"
+        },
+        {
+            "bank_account_country": "IN",
+            "currency": "INR",
+            "entity_type": "individual",
+            "purpose_code": "goods",
+            "purpose_description": "Trade settlement for goods and general goods trades"
+        },
+        {
+            "bank_account_country": "IN",
+            "currency": "INR",
+            "entity_type": "individual",
+            "purpose_code": "medical",
+            "purpose_description": "Medical treatment and expenses"
+        },
+        {
+            "bank_account_country": "IN",
+            "currency": "INR",
+            "entity_type": "individual",
+            "purpose_code": "advisor_fees",
+            "purpose_description": "Fees for advisory, technical, academic or specialist assistance"
+        },
+        {
+            "bank_account_country": "IN",
+            "currency": "INR",
+            "entity_type": "individual",
+            "purpose_code": "loan_repayment",
+            "purpose_description": "Repayment of loans"
+        },
+        {
+            "bank_account_country": "IN",
+            "currency": "INR",
+            "entity_type": "individual",
+            "purpose_code": "insurance_claims",
+            "purpose_description": "Insurance claims payment"
+        },
+        {
+            "bank_account_country": "IN",
+            "currency": "INR",
+            "entity_type": "individual",
+            "purpose_code": "construction",
+            "purpose_description": "Construction costs/expenses"
+        },
+        {
+            "bank_account_country": "IN",
+            "currency": "INR",
+            "entity_type": "individual",
+            "purpose_code": "transfer",
+            "purpose_description": "Transfer to own account"
+        },
+        {
+            "bank_account_country": "IN",
+            "currency": "INR",
+            "entity_type": "individual",
+            "purpose_code": "advertising",
+            "purpose_description": "Advertising and public relations-related expenses"
+        },
+        {
+            "bank_account_country": "IN",
+            "currency": "INR",
+            "entity_type": "individual",
+            "purpose_code": "property_purchase",
+            "purpose_description": "Purchase of residential property"
+        },
+        {
+            "bank_account_country": "IN",
+            "currency": "INR",
+            "entity_type": "individual",
+            "purpose_code": "exports",
+            "purpose_description": "Payments for exported goods"
+        },
+        {
+            "bank_account_country": "IN",
+            "currency": "INR",
+            "entity_type": "individual",
+            "purpose_code": "insurance_premium",
+            "purpose_description": "Insurance premium"
+        },
+        {
+            "bank_account_country": "IN",
+            "currency": "INR",
+            "entity_type": "individual",
+            "purpose_code": "share_investment",
+            "purpose_description": "Investment in shares"
+        },
+        {
+            "bank_account_country": "IN",
+            "currency": "INR",
+            "entity_type": "individual",
+            "purpose_code": "hotel",
+            "purpose_description": "Hotel accommodation"
+        },
+        {
+            "bank_account_country": "IN",
+            "currency": "INR",
+            "entity_type": "individual",
+            "purpose_code": "other_fees",
+            "purpose_description": "Broker, front end, commitment, guarantee and custodian fees"
+        },
+        {
+            "bank_account_country": "IN",
+            "currency": "INR",
+            "entity_type": "individual",
+            "purpose_code": "utilities",
+            "purpose_description": "Utility bills"
+        },
+        {
+            "bank_account_country": "IN",
+            "currency": "INR",
+            "entity_type": "individual",
+            "purpose_code": "family",
+            "purpose_description": "Family maintenance"
+        },
+        {
+            "bank_account_country": "IN",
+            "currency": "INR",
+            "entity_type": "individual",
+            "purpose_code": "tax",
+            "purpose_description": "Tax payment"
+        },
+        {
+            "bank_account_country": "IN",
+            "currency": "INR",
+            "entity_type": "individual",
+            "purpose_code": "transportation",
+            "purpose_description": "Transportation fees for goods"
+        },
+        {
+            "bank_account_country": "IN",
+            "currency": "INR",
+            "entity_type": "individual",
+            "purpose_code": "fund_investment",
+            "purpose_description": "Mutual fund investment"
+        },
+        {
+            "bank_account_country": "IN",
+            "currency": "INR",
+            "entity_type": "individual",
+            "purpose_code": "royalties",
+            "purpose_description": "Royalty, trademark, patent and copyright fees"
+        }
+        ]
+        }';
+
+        $entryPoint = new ReferenceEntryPoint(
+            $this->getMockedClient(
+                json_decode($data),
+                'GET',
+                'reference/payment_purpose_codes',
+                [
+                    'currency' => "INR",
+                    'entity_type' => "individual",
+                    'bank_account_country' => "IN"
+                ]
+            )
+        );
+
+        $purposeCodes = $entryPoint->paymentPurposeCodes("INR", "individual", "IN");
+
+        $this->assertSame(25, count($purposeCodes));
+
+        $this->assertSame("INR", $purposeCodes[0]->getCurrency());
+        $this->assertSame("individual", $purposeCodes[0]->getEntityType());
+        $this->assertSame("travel", $purposeCodes[0]->getPurposeCode());
+        $this->assertSame("Travel", $purposeCodes[0]->getPurposeDescription());
+    }
 }
